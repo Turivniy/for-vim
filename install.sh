3be31f0c6375aac23e91b821bb896c329f949515
Sergey Turivnyi <sturivnyi@mirantis.com>
2015-12-14 17:53:26 +0200

    Delete git installation in install.sh

--- install.sh
+++ install.sh
@@ -2,13 +2,10 @@
 
 set -e
 
-sudo apt-get install git
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 cp .vimrc ~/.vimrc
-
 mkdir -p ~/.vim/colors/
-
 cp jellybeans.vim ~/.vim/colors/
 
 vim +PluginInstall +qall
